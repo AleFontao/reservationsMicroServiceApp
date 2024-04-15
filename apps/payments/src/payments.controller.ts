ee8bf2c9a95f4e9b41e0c93ea4fdb0f51229e554
Alejo Fontao <alefonta@hotmail.com.ar>
2024-04-15 12:25:07 -0300

    Stripe

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDTO } from './dto/create-charge.dto';
 
 @Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.paymentsService.getHello();
+  @Post()
+  @MessagePattern('create_charge')
+  async createCharge(@Payload() data: CreateChargeDTO){
+    return this.paymentsService.createCharge(data);
   }
 }
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreateChargeDTO } from './dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly stripe = new Stripe(this.configService.get("STRIPE_SECRET_KEY"), {
+    apiVersion: "2024-04-10"
+  });
+
+  constructor(private readonly configService: ConfigService) {
+
+  }
+
+  async createCharge({ card, amount } : CreateChargeDTO) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card: card
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    })
+
+    return paymentIntent;
   }
 }
